133752cf8a0a89df657f084e645a779a413d59bd
Daniel John Baynosa <danieljohnbyns@gmail.com>
2025-09-04 13:31:53 +0800

    Implement code changes to enhance functionality and improve performance

--- src/pages/Authentication.jsx
+++ src/pages/Authentication.jsx
@@ -7,6 +7,11 @@ import {
 	Text,
 	SafeAreaView
 } from 'react-native';
+import { Image } from 'expo-image';
+
+import { Flex, Card } from '@ant-design/react-native';
+
+import LogoBanser from '../../assets/public/Logo Banner.png';
 
 const Authentication = () => {
 	return (
@@ -18,9 +23,29 @@ const Authentication = () => {
 			}}
 		>
 			<StatusBar style='auto' />
-
 			<Provider>
-				<Text>Authentication Page</Text>
+				<Flex
+					direction='column'
+					justify='space-between'
+					style={{
+						position: 'relative',
+						width: '100%',
+						height: '100%',
+						padding: 32
+					}}
+				>
+					<Image
+						source={LogoBanser}
+						style={{
+							width: '100%',
+							maxWidth: 512,
+							height: 128,
+							objectFit: 'contain',
+						}}
+						contentFit='contain'
+					/>
+					<Text>Copyright © Colegio de Montalban 2025</Text>
+				</Flex>
 			</Provider>
 		</SafeAreaView>
 	);
