24bbb079d920d241ef3983d6943428ef1923df61
Daniel John Baynosa <danieljohnbyns@gmail.com>
2025-10-31 12:59:53 +0800

    chore: update package version to 1.0.47, add @expo/vector-icons dependency, and enhance Home component with like functionality

--- src/pages/Feed/Tabs/Home.jsx
+++ src/pages/Feed/Tabs/Home.jsx
@@ -1,8 +1,9 @@
 import React from 'react';
-import { Image } from 'react-native';
+import { Image, Pressable } from 'react-native';
 import { RefreshControl } from 'react-native-gesture-handler';
+import Ionicons from '@expo/vector-icons/Ionicons';
 
-import { Flex, View } from '@ant-design/react-native';
+import { Flex } from '@ant-design/react-native';
 import Markdown from 'react-native-markdown-display';
 
 import Text from '../../../components/Text';
@@ -16,6 +17,8 @@ import { useRefresh } from '../../../contexts/useRefresh';
 import { API_Route } from '../../../main';
 
 import theme from '../../../styles/theme';
+import IconButton from '../../../components/IconButton';
+import authFetch from '../../../utils/authFetch';
 const Home = () => {
 	const greeting = React.useMemo(() => {
 		const hour = new Date().getHours();
@@ -76,50 +79,79 @@ const Home = () => {
  * @type {React.FC<{announcement: import('../../../classes/Announcement').AnnouncementProps}>}
  */
 const Announcement = ({ announcement }) => {
+	const [liked, setLiked] = React.useState(false);
+
+	const { cache } = useCache();
+
+	const like = async () => {
+		if (!cache.user) return;
+		setLiked(!liked);
+
+		const response = await authFetch(`${API_Route}/announcements/${announcement.id}/like`, {
+			method: liked ? 'DELETE' : 'POST',
+			signal: new AbortController().signal
+		});
+		if (response?.status === 0) return;
+		if (!response?.ok) return;
+	};
+
 	return (
-		<Flex
-			direction='column'
-			justify='flex-start'
-			align='stretch'
-			style={{
-				gap: theme.v_spacing_lg,
-				padding: theme.h_spacing_md,
-				borderBottomColor: theme.border_color_base,
-				borderBottomWidth: theme.border_width_sm,
-				backgroundColor: theme.fill_base
-			}}
-		>
-			<Flex direction='column' justify='flex-start' align='stretch'>
-				<Title level={4}>{announcement.title}</Title>
-				<Markdown>{announcement.content.split('\n')[0]}</Markdown>
-			</Flex>
-
-			<Image
-				source={{ uri: announcement.cover }}
+		<Pressable android_ripple={{ color: theme.fill_mask }}>
+			<Flex
+				direction='column'
+				justify='flex-start'
+				align='stretch'
 				style={{
-					width: '100%',
-					height: 128,
-					borderRadius: 8
+					gap: theme.v_spacing_lg,
+					borderBottomColor: theme.border_color_base,
+					borderBottomWidth: theme.border_width_sm,
+					backgroundColor: theme.fill_base
 				}}
-				resizeMode='cover'
-			/>
-
-			<Flex direction='row' justify='between' align='center'>
-				<Flex direction='row' align='center' gap={8}>
-					<Avatar
-						size='small'
-						uri={announcement.author.profilePicture}
-					/>
+			>
+				<Flex direction='column' justify='flex-start' align='stretch' style={{ paddingHorizontal: theme.h_spacing_md, paddingTop: theme.v_spacing_md }}>
+					<Title level={4}>{announcement.title}</Title>
+					<Markdown>{announcement.content.split('\n')[0]}</Markdown>
+				</Flex>
+				<Image
+					source={{ uri: announcement.cover }}
+					style={{
+						width: '100%',
+						height: 128,
+						borderRadius: 8
+					}}
+					resizeMode='cover'
+				/>
+				<Flex direction='row' justify='between' align='center' style={{ paddingHorizontal: theme.h_spacing_md }}>
+					<Flex direction='row' align='center' gap={8}>
+						<Avatar
+							size='small'
+							uri={announcement.author.profilePicture}
+						/>
+						<Text>
+							{announcement.author.name.first}{' '}
+							{announcement.author.name.last}
+						</Text>
+					</Flex>
 					<Text>
-						{announcement.author.name.first}{' '}
-						{announcement.author.name.last}
+						{new Date(announcement.created_at).toLocaleDateString()}
 					</Text>
 				</Flex>
-				<Text>
-					{new Date(announcement.created_at).toLocaleDateString()}
-				</Text>
+				<Flex direction='row' justify='between' align='center' gap={16}>
+					<Pressable onPress={like} android_ripple={{ color: theme.fill_mask, borderless: true }} style={{ backgroundColor: 'transparent', paddingHorizontal: theme.h_spacing_md, paddingBottom: theme.v_spacing_sm }}>
+						<Flex direction='row' justify='center' align='center' gap={8}>
+							<Ionicons name={liked ? 'heart' : 'heart-outline'} />
+							<Text>{liked ? announcement.likes?.length + 1 : announcement.likes?.length} like{announcement.likes?.length !== 1 && 's'}</Text>
+						</Flex>
+					</Pressable>
+					<Pressable android_ripple={{ color: theme.fill_mask, borderless: true }} style={{ backgroundColor: 'transparent', paddingHorizontal: theme.h_spacing_md, paddingVertical: theme.v_spacing_sm }}>
+						<Flex direction='row' justify='center' align='center' gap={8}>
+							<Ionicons name='chatbubble-outline' />
+							<Text>{announcement.comments?.length} comment{announcement.comments?.length !== 1 && 's'}</Text>
+						</Flex>
+					</Pressable>
+				</Flex>
 			</Flex>
-		</Flex>
+		</Pressable>
 	);
 };
 
